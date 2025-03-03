028ca614e4c8d108c733fbcf882ee99ab8855cd6
hakikul1234 <thehakikul@gmail.com>
2025-03-03 23:34:08 +0530

    Add files via upload

---
+++ assingment.py
@@ -0,0 +1,4 @@
+x=input('Enter first name:')
+y=input('Enter last name:')
+
+print('Hello ' + x + y + '!  Welcome to python program.')
---
+++ project2.py
@@ -0,0 +1,7 @@
+a=int(input('Enter any value:'))
+b=int(input('Enter any value:'))
+
+print('Addition',a + b)
+print('substraction',a - b)
+print('Multiplication',a * b)
+print('Division',a / b)
